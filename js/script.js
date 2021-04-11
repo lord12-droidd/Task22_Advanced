e2f04c2f310f3a918f6a61b8b51740713161608b
Dmytro Melnyk <melnikdmitriy2002@gmail.com>
2021-04-11 22:34:20 +0300

    added validation for float numbers

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ function CreateTable() {
     let row = document.querySelector(".rows").value
     let column = document.querySelector(".columns").value
     let table = document.createElement('table')
+    if(!Number.isInteger(parseFloat(row)) || !Number.isInteger(parseFloat(column))){
+        alert("Rows and Columns quantity must be an integer value")
+        return
+    }
     if(row < 2 || column < 2){
         alert("Rows and Columns quantity must be more than 2")
         return
